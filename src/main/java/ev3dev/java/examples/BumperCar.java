afe18873eb138bec8277f96e62218053f03bd862
Lukasz Czarnecki <lczarnecki@gmail.com>
2016-09-02 14:54:24 +0200

    What is happenin?

--- src/main/java/ev3dev/java/examples/BumperCar.java
+++ src/main/java/ev3dev/java/examples/BumperCar.java
@@ -22,53 +22,61 @@ public class BumperCar {
             System.out.println("OBRACAM!");
             mA.setSpeed(500);
             mA.forward();
-            Delay.msDelay(1000);
-            mA.stop();
+            for (int i = 0; i < 100; i++) {
+                System.out.println("ROTACJA " + mA.getPosition() +
+                        " RYCHLOSC " + mA.getSpeed() +
+                        " RYCHLOSC KATOWA " + mA.getRotationSpeed());
+                Delay.msDelay(100);
+            }
+//            Delay.msDelay(1000);
+//            mA.stop();
 //            mA.setSpeed(10);
 //            mA.backward();
 //            Delay.msDelay(1000);
 //            mA.stop();
+            mA.stop();
+            mA.close();
             System.out.println("JESZCZE JAK!");
         } catch (Exception e) {
             System.out.println("SMUTNA ZABA!!");
             e.printStackTrace();
         }
-        try {
-            System.out.println("SPRAWDZAM MOTUR B!");
-            mB = new EV3LargeRegulatedMotor(MotorPort.B);
-            System.out.println("MAMY TO!");
-            System.out.println("OBRACAM!");
-            mB.setSpeed(500);
-            mB.forward();
-            Delay.msDelay(1000);
-            mB.stop();
-//            mB.setSpeed(10);
-//            mB.backward();
-//            Delay.msDelay(1000);
-//            mB.stop();
-            System.out.println("JESZCZE JAK!");
-        } catch (Exception e) {
-            System.out.println("SMUTNA ZABA!!");
-            e.printStackTrace();
-        }
-        try {
-            System.out.println("SPRAWDZAM MOTUR C!");
-            mC = new EV3MediumRegulatedMotor(MotorPort.C);
-            System.out.println("MAMY TO!");
-            System.out.println("OBRACAM!");
-            mC.setSpeed(500);
-            mC.forward();
-            Delay.msDelay(1000);
-            mC.stop();
-//            mB.setSpeed(10);
-//            mB.backward();
-//            Delay.msDelay(1000);
-//            mB.stop();
-            System.out.println("JESZCZE JAK!");
-        } catch (Exception e) {
-            System.out.println("SMUTNA ZABA!!");
-            e.printStackTrace();
-        }
+//        try {
+//            System.out.println("SPRAWDZAM MOTUR B!");
+//            mB = new EV3LargeRegulatedMotor(MotorPort.B);
+//            System.out.println("MAMY TO!");
+//            System.out.println("OBRACAM!");
+//            mB.setSpeed(500);
+//            mB.forward();
+////            Delay.msDelay(1000);
+////            mB.stop();
+////            mB.setSpeed(10);
+////            mB.backward();
+////            Delay.msDelay(1000);
+////            mB.stop();
+//            System.out.println("JESZCZE JAK!");
+//        } catch (Exception e) {
+//            System.out.println("SMUTNA ZABA!!");
+//            e.printStackTrace();
+//        }
+//        try {
+//            System.out.println("SPRAWDZAM MOTUR C!");
+//            mC = new EV3MediumRegulatedMotor(MotorPort.C);
+//            System.out.println("MAMY TO!");
+//            System.out.println("OBRACAM!");
+//            mC.setSpeed(500);
+//            mC.forward();
+////            Delay.msDelay(1000);
+////            mC.stop();
+////            mB.setSpeed(10);
+////            mB.backward();
+////            Delay.msDelay(1000);
+////            mB.stop();
+//            System.out.println("JESZCZE JAK!");
+//        } catch (Exception e) {
+//            System.out.println("SMUTNA ZABA!!");
+//            e.printStackTrace();
+//        }
 
 
         System.out.println("NARA!!");
