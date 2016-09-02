ead27c778cf69803184296b1cefaba9bbcea33e9
Lukasz Czarnecki <lczarnecki@gmail.com>
2016-09-02 14:30:30 +0200

    Do something!

--- src/main/java/ev3dev/java/examples/BumperCar.java
+++ src/main/java/ev3dev/java/examples/BumperCar.java
@@ -2,6 +2,7 @@ package ev3dev.java.examples;
 
 import ev3dev.hardware.motor.EV3LargeRegulatedMotor;
 import ev3dev.hardware.port.MotorPort;
+import lejos.utility.Delay;
 
 public class BumperCar {
     
@@ -16,6 +17,14 @@ public class BumperCar {
             System.out.println("SPRAWDZAM MOTUR A!");
             mA = new EV3LargeRegulatedMotor(MotorPort.A);
             System.out.println("MAMY TO!");
+            System.out.println("OBRACAM!");
+            mA.setSpeed(10);
+            mA.rotate(90);
+            Delay.msDelay(1000);
+            mA.setSpeed(-10);
+            mA.rotate(90);
+            Delay.msDelay(1000);
+            System.out.println("JESZCZE JAK!");
         } catch (Exception e) {
             System.out.println("SMUTNA ZABA!!");
             e.printStackTrace();
@@ -24,6 +33,14 @@ public class BumperCar {
             System.out.println("SPRAWDZAM MOTUR B!");
             mB = new EV3LargeRegulatedMotor(MotorPort.B);
             System.out.println("MAMY TO!");
+            System.out.println("OBRACAM!");
+            mB.setSpeed(10);
+            mB.rotate(90);
+            Delay.msDelay(1000);
+            mB.setSpeed(-10);
+            mB.rotate(90);
+            Delay.msDelay(1000);
+            System.out.println("JESZCZE JAK!");
         } catch (Exception e) {
             System.out.println("SMUTNA ZABA!!");
             e.printStackTrace();
